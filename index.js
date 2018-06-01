2cc12abb9554058c480450986b9c70b909026a70
Nicholas Smith <npsmith1990@gmail.com>
2018-06-01 12:29:50 -0600

    Ability to register users and login

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const app = require("express");
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-//var { db, fetchMessages, insertMessage } = require('./mongo_db.js')
+
+const LOGIN_READY = "LOGIN_READY"
+const GREETING = "GREETING"
+const REGISTER_ATTEMPT = "REGISTER_ATTEMPT"
+const REGISTER_SUCCESS = "REGISTER_SUCCESS"
+const REGISTER_FAIL = "REGISTER_FAIL"
+const LOGIN_ATTEMPT = "LOGIN_ATTEMPT"
+const LOGIN_FAIL = "LOGIN_FAIL"
+const LOGIN_SUCCESS = "LOGIN_GOOD"
+
+var {
+  db,
+  fetchMessages,
+  insertMessage,
+  registerUser,
+  loginUser,
+} = require('./mongo_db.js')
 
 var PORT = 3000
 
@@ -9,20 +25,40 @@ var PORT = 3000
 // Socket Server
 io.on("connection", (socket) => {
   console.log("A user connected.");
-  socket.on("greeting", (data) => {
-    console.log(data)
-    socket.emit("reply", "Hello " + data);
+  socket.on(GREETING, (data) => {
+    socket.emit("LOGIN_READY", data);
   });
 
-  const interval = setInterval(() => {
-    const message = `ping @ ${new Date().toLocaleTimeString()}`
-    socket.emit("reply", message)
-    console.log(message)
-  }, 100)
+  socket.on(REGISTER_ATTEMPT, data => {
+    var parsed = JSON.parse(data)
+    const { username, password } = parsed
+    // Attempt to create this user in Mongodb
+    registerUser(parsed, (res)=> {
+      if (res === "success") {
+        socket.emit(REGISTER_SUCCESS)
+        console.log(REGISTER_SUCCESS)
+      } else {
+        socket.emit(REGISTER_FAIL)
+        console.log(REGISTER_FAIL)
+      }
+    })
+  })
+
+  socket.on(LOGIN_ATTEMPT, data => {
+    var parsed = JSON.parse(data)
+    const { username, password } = parsed
+    // @TODO: Attempt to log this user in
+    loginUser(parsed, (success, user) => {
+      if (success) {
+        socket.emit(LOGIN_SUCCESS, JSON.stringify(user))
+      } else {
+        socket.emit(LOGIN_FAIL)
+      }
+    })
+  })
 
   socket.on('disconnect', () => {
-    clearInterval(interval)
-    console.log('A user left.')
+    console.log(`A user left: ${socket.id}`)
   })
 });
 
--- mongo_db.js
+++ mongo_db.js
@@ -6,7 +6,7 @@ const dbName = 'mmodb'
 let db = null
 
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
-  assert.equal(null, err)
+  assert.equal(err, null)
   db = client.db(dbName)
 })
 
@@ -27,9 +27,48 @@ const insertMessage = (message, callback) => {
   })
 }
 
+const registerUser = (data, callback) => {
+  const { username, password } = data
+  const collection = db.collection('users')
+
+  collection.findOne({ username }, (err, res) => {
+    assert.equal(err, null)
+    if (res == null) {
+      collection.insert({
+        username,
+        password,
+        sprite: 'BeardGuy_down',
+        map: 'rm_map_home',
+        x: 320,
+        y: 320,
+      }, (err, res) => {
+        assert.equal(err, null)
+        callback('success')
+      })
+    } else {
+      callback("User exists")
+    }
+  })
+}
+
+const loginUser = (data, callback) => {
+  const { username, password } = data
+  const collection = db.collection('users')
+
+  collection.findOne({ username }, (err, user) => {
+    if (!err && user) {
+      if (user.password = password) {
+        return callback(true, user)
+      }
+    }
+    callback(false, null)
+  })
+}
 
 module.exports = {
   db,
   fetchMessages,
   insertMessage,
+  registerUser,
+  loginUser,
 }
